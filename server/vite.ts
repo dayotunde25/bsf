bd0eb405161936829fb76da386225ff1b9ed1c90
Babatunde Dayo <dayotunde25@gmail.com>
2025-07-30 09:09:34 +0100

    Correct login/register issue

--- server/vite.ts
+++ server/vite.ts
@@ -68,7 +68,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(import.meta.dirname, "public");
+  const distPath = path.resolve(process.cwd(), "dist/client");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -79,7 +79,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
